41188a859b92d7ce90682a085364ef8fa40fb2f1
Hector <hec.zenemij@gmail.com>
2023-02-07 20:30:44 -0500

    practica2

---
+++ assets/js/update.js
@@ -0,0 +1,29 @@
+
+const runScripts = () => {
+
+    let chart_bars = document.getElementById("chart-bars");
+    console.log( chart_bars );
+
+    let icons  = document.getElementsByTagName("i");
+    console.log( icons );
+
+    let cards  = document.getElementsByClassName("card");
+    console.log( cards );
+}
+
+runScripts();
+
+
+
+const modifyText = () => {
+
+    let listOfP  = document.getElementsByTagName("p");
+    let elementP = listOfP[3];
+    elementP.innerHTML = 'Dinero actual';
+
+    let listOfH4 = document.getElementsByTagName("h4");
+    let elementH4 = listOfH4[0];
+    elementH4.innerHTML = '$301k';
+}
+
+modifyText();
